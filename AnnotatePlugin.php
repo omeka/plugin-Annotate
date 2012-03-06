4beeb96e170d79f0753e97c9454ebd04bbf3dc67
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-03-06 10:03:26 -0500

    Removes deprecated $db->exec()

--- AnnotatePlugin.php
+++ AnnotatePlugin.php
@@ -4,10 +4,10 @@
  * @copyright Center for History and New Media, 2012
  * @package Contribution
  */
- 
+
 /**
  * Annotate plugin class
- * 
+ *
  * @package Annotate
  */
 class AnnotatePlugin extends Omeka_Plugin_Abstract {
@@ -32,9 +32,9 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
     'annotation_page_title' => 'Annotations',
     'annotation_bookmarks'  => 'Bookmarks'
   );
-   
+
   public function hookInstall(){
-   
+
     //create the query for the table
     $sql = "CREATE TABLE IF NOT EXISTS `{$this->_db->Annotate}` (
               `id`       BIGINT UNSIGNED NOT NULL auto_increment PRIMARY KEY,
@@ -42,49 +42,49 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
               `user_id`  BIGINT UNSIGNED NOT NULL,
               `item_id`  BIGINT UNSIGNED NOT NULL,
               `bookmark` TINYINT NOT NULL DEFAULT 0,
-              `modified` TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP  
+              `modified` TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
             ) ENGINE = MYISAM;";
-            
-    $this->_db->exec($sql);
-    
+
+    $this->_db->query($sql);
+
     $this->_installOptions();
   }
-  
+
   public function hookUninstall(){
-  
+
     $sql = "DROP TABLE IF EXISTS `{$this->_db->Annotate}`";
-    $this->_db->exec($sql);
-    
+    $this->_db->query($sql);
+
     $this->_uninstallOptions();
   }
-  
+
   public function hookConfig($post){
-    $pagePath = !empty($post['annotation_page_path']) 
-                ? trim($post['annotation_page_path']) 
+    $pagePath = !empty($post['annotation_page_path'])
+                ? trim($post['annotation_page_path'])
                 : $this->_options['annotation_page_path'];
     set_option('annotation_page_path',$pagePath);
-    
+
     $pageTitle = !empty($post['annotation_page_title'])
                  ? trim($post['annotation_page_title'])
                  : $this->_options['annotation_page_title'];
     set_option('annotation_page_title',$pageTitle);
-    
+
     $bookmark = !empty($post['annotation_bookmarks'])
                 ? trim($post['annotation_bookmarks'])
                 : $this->_options['annotation_bookmarks'];
     set_option('annotation_bookmarks',$bookmark);
   }
-  
+
   public function hookConfigForm(){
     $pagePath = get_option('annotation_page_path');
     $pageTitle = get_option('annotation_page_title');
     $bookmark = get_option('annotation_bookmarks');
-    
+
     include 'forms/config_form.php';
   }
-  
+
   public function hookDefineRoutes($router){
-  
+
   $router->addRoute(
             'annotationDefaultRoute',
             new Zend_Controller_Router_Route(
@@ -96,7 +96,7 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
                     )
             )
         );
-        
+
         if ($bp = get_option('annotation_page_path')) {
             $router->addRoute(
                 'annotationCustomRoute',
@@ -106,9 +106,9 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
                           'controller' => 'index',
                           'action'     => 'browse')));
         }
-    
+
   }
-  
+
   public function hookPublicAppendToItemsShow(){
     if($user = current_user()){
       $bp = get_option('annotation_page_path');
@@ -116,14 +116,14 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
       $annotation = annotate_get_user_note_for_item();
       $tags = tag_string(current_user_tags_for_item());
       $bookmark = get_option('annotation_bookmarks');
-      
+
       include 'forms/public_form.php';
     }
   }
-  
+
   public function filterGuestUserWidgets($widgets){
     $user = current_user();
-    
+
     if($user){
       //Dashboard Title
       $bp = get_option('annotation_page_path');
@@ -131,7 +131,7 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
       $bb = get_option('annotation_bookmarks');
       $html = "<h2><a href=\"".html_escape(uri($bp))."\">$bt $bb</a></h2><hr>";
       $note = annotate_get_items_and_notes_by_user_favorite($user,1);
-      
+
       //place the html in the $widgets variable
       foreach($note as $n){
         $item = get_item_by_id($n->item_id);
@@ -145,24 +145,24 @@ class AnnotatePlugin extends Omeka_Plugin_Abstract {
     $widgets['notes'] = $html;
     return $widgets;
   }
-  
+
   public function filterPublicNavigationMain($nav){
-   
+
     if(!plugin_is_active('GuestUser')){
       if($user= current_user()){
        $path = get_option('annotation_page_path');
        $nav['Notes'] = uri($path);
        $nav['logout'] = uri('users/logout');
-    
+
       }else{
         $nav['login'] = uri('users/login');
       }
     }
     return $nav;
   }
-  
+
   public function hookPublicThemeHeader($request){
     queue_css('note');
   }
-  
+
 }
